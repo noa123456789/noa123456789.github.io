81832c7e394c377de8a901afee438702c2bd201c
noa123456789 <105967219+noa123456789@users.noreply.github.com>
2023-12-31 20:45:37 +0100

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+const socket = io();
+
 function sendMessage() {
     const messageInput = document.getElementById('message-input');
     const message = messageInput.value;
 
     if (message.trim() !== '') {
-        const chatDisplay = document.getElementById('chat-display');
-        const newMessage = document.createElement('div');
-        newMessage.textContent = message;
-        chatDisplay.appendChild(newMessage);
-
-        // Voeg hier de logica toe om het bericht naar de server te sturen (AJAX, WebSockets, etc.).
+        // Verzend het bericht naar de server.
+        socket.emit('message', { message });
     }
 
     messageInput.value = '';
 }
+
+socket.on('message', (data) => {
+    // Ontvang en verwerk berichten van de server.
+    console.log('Message from ' + data.id + ': ' + data.message);
+});
